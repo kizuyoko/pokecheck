0bee689cf3aadb8b61d7340e61beb1ac3437ecfe
kizuyoko <kizuyoko@gmail.com>
2025-04-22 18:20:46 +0200

    vercel adjustment

--- src/app/pokemon/[name]/page.tsx
+++ src/app/pokemon/[name]/page.tsx
@@ -1,13 +1,26 @@
 import { notFound } from 'next/navigation';
 import pokemonList from "@/data/pokemonList";
 import PokemonProfileClient from "./PokemonProfileClient";
+import { Metadata } from "next";
 
+interface PageProps {
+  params: Promise<{ name: string }>;
+}
+// This interface is a promise that resolves to an object with a name property of type string. But actually, it is not nessesary to use a promise here, because the params are already a promise in the function signature. If you get error after update the Vercel version, you can use the following code instead (Some async/await functions below are not necessary neither, don't forget to fix it in search/page.tsx as well):
+
+/* 
+interface PageProps {
+  params: { name: string }; 
+}
+*/  
 export async function generateStaticParams() {
   return pokemonList.map((p) => ({ name: p.name.toLowerCase() }));
 }
 
-export async function generateMetadata({ params }: { params: { name: string } }) {
-  const name = decodeURIComponent(params.name)
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const { name: rawName } = await params;
+  //await params is used because of temporary promise in the function signature. If you use the above code, you can just use params.name directly.
+  const name = decodeURIComponent(rawName)
     .toLowerCase()
     .replace(/^\w/, (c) => c.toUpperCase());
 
