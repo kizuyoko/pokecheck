422d94d9519d1d0e7d056f9c47e85a06853e61c3
kizuyoko <kizuyoko@gmail.com>
2025-09-16 14:16:17 +0200

    Pokemon list switch to own server

--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,7 +1,10 @@
+'use client';
 import PokemonList from "../components/List/PokemonList";
-import { pokemonNameID } from "@/data/pokemonNameID";
+import { usePokemons } from "@/lib/hooks/usePokemons";
 
 const PokemonIndexPage = () => {
+  const { data: pokemons = [] } = usePokemons();
+  const pokemonNameID = pokemons; 
   return (
     <>
       <h2>Pokémon</h2>
