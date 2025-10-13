4fee6401a270e8a35e523ff528728bb40590593a
kizyoko <kizuyoko@gmail.com>
2025-10-13 15:38:07 +0200

    Non Existent Pokemon

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -4,14 +4,21 @@ describe("Search Pokemon", () => {
     
     cy.get('[data-testid="search-input"]').type("Pikachu{enter}");
 
-
     // Wait for the search results to load and display Pikachu
-    cy.contains("Pikachu", { timeout: 20000 }).should("be.visible");
+    cy.contains("Pikachu", { timeout: 5000 }).should("be.visible");
 
     // Click on the Pikachu link to navigate to its detail page
     cy.contains("#0025").click();
 
     // Verify that the Pikachu detail page is displayed
-    cy.contains("Pikachu", { timeout: 10000 }).should("be.visible");
+    cy.contains("Pikachu", { timeout: 5000 }).should("be.visible");
+  });
+
+  it("shows no results for a non-existent Pokemon", () => {
+    cy.visit("http://localhost:3000");
+    cy.get('[data-testid="search-input"]').type("NonExistentPokemon{enter}");
+
+    // Verify that no results message is displayed
+    cy.contains("Not Found", { timeout: 5000 }).should("be.visible");
   });
 });
