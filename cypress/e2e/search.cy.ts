6e74cb4191317d5f311dba73a9787f945a85f187
Yoko Kizu <kizuyoko@gmail.com>
2025-11-03 13:40:08 +0100

    Waiting time for Pikachu to 10000ms from 5000ms

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -11,7 +11,7 @@ describe("Search Pokemon", () => {
     cy.contains("Pikachu", { timeout: 10000 }).should("be.visible");
 
     // Click on the Pikachu link to navigate to its detail page
-    cy.contains("#0025", { timeout: 5000 }).should("be.visible").click();
+    cy.contains("#0025", { timeout: 10000 }).should("be.visible").click();
 
     // Verify that the Pikachu detail page is displayed
     cy.contains("Pikachu", { timeout: 10000 }).should("be.visible");
